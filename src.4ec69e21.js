ddc89ebc5801b3b169604c8a99249d573533750f
AnnaFesenko7 <90966976+AnnaFesenko7@users.noreply.github.com>
2022-05-18 16:31:20 +0000

    Deploying to gh-pages from @ AnnaFesenko7/goit-markup-BarberShop@dc72808546a1c84de9d2fe7b4c6f0156a842a4bd 🚀

--- src.4ec69e21.js
+++ src.4ec69e21.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("animate.css");
 },{"./sass/main.scss":"clu1","animate.css":"TOx8"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-markup-BarberShop/src.7e5fc666.js.map
+//# sourceMappingURL=/goit-markup-BarberShop/src.4ec69e21.js.map
