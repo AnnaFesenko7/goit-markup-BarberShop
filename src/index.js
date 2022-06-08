57a2eb98911f397281903b5a852247fb721c0417
AnnaFesenko7 <annafecenko7@gmail.com>
2022-06-08 06:54:17 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { manageModalWindow } from './js/modal-window';
 import { manageNavigation } from './js/navigation';
 import { manageModalMenu } from './js/modal-menu';
 import { manageMenuNavigation } from './js/menu-navigation';
-// import Siema from 'siema';
+import Siema from 'siema';
 
 manageModalWindow();
 manageNavigation();
 manageModalMenu();
 manageMenuNavigation();
 
-// const mySiema = new Siema();
-// document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
-// document.querySelector('.next').addEventListener('click', () => mySiema.next());
+const mySiema = new Siema();
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
